57bf2715b58a68d042c826b5640aee8e189d15cf
baker.lee <zeemoong@github.com>
2023-04-03 22:46:56 +0900

    init.

---
+++ src/main/java/com/baker1ee/pastry/PastryApplication.java
@@ -0,0 +1,13 @@
+package com.baker1ee.pastry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baker1ee/pastry/PastryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baker1ee.pastry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
