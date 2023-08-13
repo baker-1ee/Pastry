c3ed1333b299a57d8c247faeefce1a40fbc8eed1
baker.lee <zeemoong@github.com>
2023-08-13 17:31:46 +0900

    CORS 매핑 추가

---
+++ src/main/java/com/baker1ee/pastry/common/WebConfig.java
@@ -0,0 +1,15 @@
+package com.baker1ee.pastry.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:7777")
+                .allowCredentials(true);
+    }
+}
