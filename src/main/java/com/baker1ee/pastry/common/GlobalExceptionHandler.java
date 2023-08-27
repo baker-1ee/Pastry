85f2b349588a37410468608ecc00dadb520cc0f7
baker.lee <zeemoong@github.com>
2023-08-27 18:38:46 +0900

    feat. ExceptionHandler 설정

--- src/main/java/com/baker1ee/pastry/common/GlobalExceptionHandler.java
+++ src/main/java/com/baker1ee/pastry/common/GlobalExceptionHandler.java
@@ -1,2 +1,25 @@
-package com.baker1ee.pastry.common;public class GlobalExceptionHandler {
+package com.baker1ee.pastry.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    private static class ErrorResponse {
+        private String message;
+    }
+
 }
